081bea3237fe939e0fd12d13fe4b24dc96a33e50
Thiago Marques <thiagomarquesgoes@gmail.com>
2016-10-02 15:55:37 -0300

    Navegação entre cenas

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	void OnGUI(){
+		GUI.Label(new Rect (0,0,150,20),"Para onde quer ir:");
+		if(GUI.Button(new Rect (0,20,100,50),"Ferreiro")) { Application.LoadLevel("Scenes/Shop");}
+		if(GUI.Button(new Rect (0,70,100,50),"Missão")) { Application.LoadLevel("Scenes/EnterQuest");}
+	}
+}
---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quest : MonoBehaviour {
+	void OnGUI(){
+		if(GUI.Button(new Rect (0,0,100,50),"Voltar ao mapa")) { Application.LoadLevel("Scenes/Map");}
+		GUI.Label (new Rect (0, 50, 150, 20), "Mate os ratos do porão!");
+		GUI.Label (new Rect (0, 200, 500, 20), "rato    rato    rato    rato    rato    rato    rato    rato    rato    ");
+	}
+}
