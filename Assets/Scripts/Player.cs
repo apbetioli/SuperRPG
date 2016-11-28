8b6bc254ce809f7fb271b71a5e29940ccb0047f3
Alexandre Parra Betioli <apbetioli@gmail.com>
2016-11-28 21:58:34 -0200

    Status

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private float attack;
+	private float defense;
+	private float health;
+	private float stamina;
+
+}
