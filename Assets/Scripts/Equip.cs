e258d3b98c6f57393d7b35c4f801ba5df30a96a4
Alexandre Parra Betioli <apbetioli@gmail.com>
2016-11-29 23:31:26 -0200

    Equip scene

---
+++ Assets/Scripts/Equip.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equip : MonoBehaviour {
+
+	public Player player;
+	public Weapon weapon;
+
+	public void DoEquip() {
+		if (player.CanEquipWeapon (weapon)) {
+			player.weapon = weapon;
+		}
+	}
+}
