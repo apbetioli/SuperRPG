9f868b311883e6fdec8b91dd11405692b10fdbe7
Thiago Marques <thiagomarquesgoes@gmail.com>
2017-02-14 21:54:13 -0200

    Menu

---
+++ Assets/SuperRPG/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Menu : MonoBehaviour
+{
+ 
+    public void StartGame()
+    {
+        SceneManager.LoadScene ("Load");
+    }
+    public void Exit()
+    {
+		Debug.Log("Fechar jogo!");
+        Application.Quit();
+    }
+
+	 public void Credits()
+    {
+		Debug.Log("Creditos!");
+    }
+}
---
+++ Assets/SuperRPG/Scripts/MenuButton.cs
@@ -0,0 +1,21 @@
+﻿ using UnityEngine;  
+ using System.Collections;  
+ using UnityEngine.EventSystems;  
+ using UnityEngine.UI;
+ 
+ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+ 
+     public Text theText;
+ 
+     public void OnPointerEnter(PointerEventData eventData)
+     {
+         theText.color = Color.white;
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+		Color myColor = new Color();
+      	ColorUtility.TryParseHtmlString("#414141FF", out myColor);
+        theText.color = myColor;
+     }
+ }
